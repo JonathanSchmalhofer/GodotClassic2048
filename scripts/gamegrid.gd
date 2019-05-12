7b58067b3af416bbdd85b620005165076a8469c0
JonathanSchmalhofer <jonathan.schmalhofer@gmail.com>
2019-05-12 18:19:58 +0200

    integrated SwipeDetector

--- scripts/gamegrid.gd
+++ scripts/gamegrid.gd
@@ -11,10 +11,10 @@ export (int) var numberStartingPawns
 export (int) var width
 export (int) var height
 
-# Touch Variables
-var first_touch = Vector2(0, 0);
-var final_touch = Vector2(0, 0);
-var last_direction = 0;
+# Swipe Variables
+onready var SwipeDirections = preload("res://addons/swipe-detector/directions.gd").new()
+onready var DirectionLabel = get_node("Values/Direction")
+onready var AngleLabel = get_node("Values/Angle/Value")
 
 # Piece Variables
 var possiblePawns = [
@@ -49,3 +49,40 @@ func blank_space_on_board():
 				return true
 	return false
 
+# Swiping
+
+func _on_SwipeDetector_swipe_ended( gesture ):
+	DirectionLabel.set_text(gesture.get_direction())
+	AngleLabel.set_text(str(gesture.get_direction_angle()))
+
+func _on_SwipeDetector_swiped( gesture ):
+	if(SwipeDirections.DIRECTION_UP == gesture.get_direction()):
+		_move_pawns_up()
+	else: if(SwipeDirections.DIRECTION_RIGHT == gesture.get_direction()):
+		_move_pawns_right()
+	else: if(SwipeDirections.DIRECTION_DOWN == gesture.get_direction()):
+		_move_pawns_down()
+	else: if(SwipeDirections.DIRECTION_LEFT == gesture.get_direction()):
+		_move_pawns_left()
+
+
+#          ▴ Up
+#          |
+#       [][][][]
+# Left  [][][][] Right
+#  <--  [][][][] -->
+#       [][][][]
+#          |
+#          ▾ Down
+
+func _move_pawns_up():
+	pass
+
+func _move_pawns_right():
+	pass
+
+func _move_pawns_down():
+	pass
+
+func _move_pawns_left():
+	pass
